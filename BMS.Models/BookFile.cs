5b45492ab8ca38639299f706a0dee887d5b8090b
jocogum-dev <jocogum10@gmail.com>
2026-01-24 15:22:18 +0800

    add bookfile model

---
+++ BMS.Models/BookFile.cs
@@ -0,0 +1,12 @@
+﻿namespace BMS.Models;
+
+public class BookFile
+{
+    public Guid Id { get; set; }
+    public string FileName { get; set; } = default!;
+    public string FullPath { get; set; } = default!;
+    public string DirectoryPath { get; set; } = default!;
+    public long SizeBytes { get; set; }
+    public DateTime LastModified { get; set; }
+    public DateTime AddedAt { get; set; }
+}
