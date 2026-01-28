841ab69ab70b90baeaeeb0dd1da6aa62ed40ffa3
jocogum-dev <jocogum10@gmail.com>
2026-01-28 23:06:44 +0800

    change layout to sidebar style and add view pdf in browser functionality

--- BMS.Web/Pages/Books/Details.cshtml.cs
+++ BMS.Web/Pages/Books/Details.cshtml.cs
@@ -1,3 +1,5 @@
+using BMS.Data;
+using BMS.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,23 @@ namespace BMS.Web.Pages.Books
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly BookDbContext _db;
+        public BookFile? Books { get; set; } = default!;
+        public DetailsModel(BookDbContext db)
         {
+            _db = db;
+        }
+        public async Task<IActionResult> OnGetAsync([FromQuery] Guid id)
+        {
+            Books = await _db.Books.FindAsync(id);
+            if (Books == null || !System.IO.File.Exists(Books.FullPath))
+            {
+                return NotFound();
+            }
+            return PhysicalFile(
+                Books.FullPath,
+                "application/pdf"
+            );
         }
     }
 }
