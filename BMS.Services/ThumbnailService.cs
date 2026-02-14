004b2fbe3efcd2dfe46001d306dca70f10b4b8aa
jocogum-dev <jocogum10@gmail.com>
2026-02-14 12:50:47 +0800

    change cli args in service

--- BMS.Services/ThumbnailService.cs
+++ BMS.Services/ThumbnailService.cs
@@ -6,6 +6,7 @@ public class ThumbnailService
 {
     public void GenerateThumbnail(string directoryPath,string pdfPath, string outputPath)
     {
+        var fullPdfPath = Path.Combine(directoryPath, pdfPath);
         var directory = Path.GetDirectoryName(outputPath);
         if (!Directory.Exists(directory))
         {
@@ -16,15 +17,16 @@ public class ThumbnailService
             return;
         }
         var process = new Process();
-        process.StartInfo.FileName = "/usr/bin/convert";
-        process.StartInfo.ArgumentList.Add("-density");
-        process.StartInfo.ArgumentList.Add("150");
-        process.StartInfo.ArgumentList.Add($"{directoryPath}/{pdfPath}[0]");
-        process.StartInfo.ArgumentList.Add("-quality");
-        process.StartInfo.ArgumentList.Add("90");
-        process.StartInfo.ArgumentList.Add("-resize");
-        process.StartInfo.ArgumentList.Add("300x");
-        process.StartInfo.ArgumentList.Add(outputPath);
+        process.StartInfo.FileName = "/usr/bin/gs";
+        process.StartInfo.ArgumentList.Add("-sDEVICE=jpeg");
+        process.StartInfo.ArgumentList.Add("-dFirstPage=1");
+        process.StartInfo.ArgumentList.Add("-dLastPage=1");
+        process.StartInfo.ArgumentList.Add("-dJPEGQ=85");
+        process.StartInfo.ArgumentList.Add("-r25");
+        process.StartInfo.ArgumentList.Add($"-sOutputFile={outputPath}");
+        process.StartInfo.ArgumentList.Add("-dNOPAUSE");
+        process.StartInfo.ArgumentList.Add("-dBATCH");
+        process.StartInfo.ArgumentList.Add(fullPdfPath);
         process.StartInfo.RedirectStandardError = true;
         process.StartInfo.UseShellExecute = false;
 
@@ -34,7 +36,7 @@ public class ThumbnailService
         if (process.ExitCode != 0)
         {
             var error = process.StandardError.ReadToEnd();
-            throw new Exception($"Generate thumbnail faile: {error}");
+            throw new Exception($"Generate thumbnail failed: {error}");
         }
     }
     public string GetOrGenerateThumbnail(string directoryPath, string pdfPath, Guid id)
